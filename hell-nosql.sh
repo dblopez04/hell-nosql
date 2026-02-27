f5369f28d8e6601d3a390767b2b11cfbc014d3d5
daniel <daniel@xtal.gay>
2026-02-26 21:16:57 -0600

    basic cli loop

--- hell-nosql.sh
+++ hell-nosql.sh
@@ -4,9 +4,23 @@
 DB_FILE="./data.db"
 LOG_FILE="./hnsql.log"
 
+PROMPT="$(whoami)@hnsql ~ "
+
 # Imports
+source ./lib/get.sh
+source ./lib/index.sh
 source ./lib/init.sh
+source ./lib/set.sh
 
 if [[ ! -f "$DB_FILE" || ! -f "$LOG_FILE" ]]; then
-	initial_setup
+  initial_setup
 fi
+
+while read -rp "$PROMPT" cmd key value; do
+  case $cmd in
+  EXIT)
+    echo "Exiting hnSQL"
+    exit
+    ;;
+  esac
+done
--- lib/init.sh
+++ lib/init.sh
@@ -2,7 +2,7 @@
 set -x
 
 initial_setup() {
-	echo "Welcome to Hell NoSQL, this appears to be your first time"
-	[[ ! -f "$DB_FILE" ]] && : >"$DB_FILE" && echo "Created DB file"
-	[[ ! -f "$LOG_FILE" ]] && : >"$LOG_FILE" && echo "Created log file"
+  echo "Welcome to Hell NoSQL, this appears to be your first time"
+  [[ ! -f "$DB_FILE" ]] && : >"$DB_FILE" && echo "Created DB file"
+  [[ ! -f "$LOG_FILE" ]] && : >"$LOG_FILE" && echo "Created log file"
 }
