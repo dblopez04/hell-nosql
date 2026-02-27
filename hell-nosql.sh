72ebfc0d7dd3526a8344b2d92c3dd6599fd5aa86
daniel <daniel@xtal.gay>
2026-02-26 22:26:21 -0600

    in-memory index done. i'm also just putting everything in one file cuz it was a bit annoying before. will clean everything up later

--- hell-nosql.sh
+++ hell-nosql.sh
@@ -2,17 +2,56 @@
 
 # Environment Variables
 DB_FILE="./data.db"
-
 PROMPT="$(whoami)@hnsql ~ "
+INDEX_KEYS=()
+INDEX_VALUES=()
 
-# Imports
-source ./lib/cmds.sh
-source ./lib/index.sh
-source ./lib/init.sh
+index_set() {
+  local key=$1
+  local value=$2
+  for i in "${!INDEX_KEYS[@]}"; do
+    if [[ "${INDEX_KEYS[$i]}" == "$key" ]]; then
+      INDEX_VALS[$i]="$value"
+      return
+    fi
+  done
+  INDEX_KEYS+=("$key")
+  INDEX_VALS+=("$value")
+}
+
+db_set() {
+  local key=$1
+  local value=$2
+
+  echo "SET $key $value" >>"$DB_FILE"
+}
+
+db_get() {
+  local key=$1
+
+  grep "^SET $key " data.db | tail -1 | cut -d' ' -f3
+}
+
+initial_setup() {
+  echo "Welcome to Hell NoSQL, this appears to be your first time"
+  [[ ! -f "$DB_FILE" ]] && : >"$DB_FILE" && echo "Created DB file"
+}
+
+init_index() {
+  while IFS= read -r line || [[ -n "$line" ]]; do
+    key=$(echo "$line" | cut -d' ' -f2)
+    value=$(echo "$line" | cut -d' ' -f3)
+    index_set "$key" "$value"
+  done <"$DB_FILE"
+}
+
+
 
 if [[ ! -f "$DB_FILE" ]]; then
   initial_setup
 fi
+
+init_index
 
 while read -rp "$PROMPT" cmd key value; do
   case "$cmd" in
--- lib/init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-initial_setup() {
-  echo "Welcome to Hell NoSQL, this appears to be your first time"
-  [[ ! -f "$DB_FILE" ]] && : >"$DB_FILE" && echo "Created DB file"
-}
